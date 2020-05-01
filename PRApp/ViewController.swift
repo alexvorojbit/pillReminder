909fca44eb018fff50891c2aaad9f87414aed47f
Jakub <jh.homik@gmail.com>
2020-05-01 19:57:46 +0200

    first commit

---
+++ PRApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PRApp
+//
+//  Created by Jakub Homik on 01/05/2020.
+//  Copyright © 2020 Jakub Homik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
+
